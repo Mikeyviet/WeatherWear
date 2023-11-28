f1ca42cba8e88ec3cbe1cd93fa10045bd1de84cd
Tung <dientung2000@gmail.com>
2023-11-27 23:03:41 -0600

    add clothing suggestion

--- app/src/main/java/com/bitr8/weatherwear/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/bitr8/weatherwear/ui/gallery/GalleryFragment.java
@@ -1,6 +1,7 @@
 package com.bitr8.weatherwear.ui.gallery;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,8 @@ public class GalleryFragment extends Fragment {
     private FragmentGalleryBinding binding;
     private TextView clothingText;
 
+    private ImageView clothingImage;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         GalleryViewModel galleryViewModel =
@@ -37,6 +40,7 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         clothingText = root.findViewById(R.id.text_clothing);
+        clothingImage = root.findViewById(R.id.imageView2);
         Bundle bundle = this.getArguments();
         displayClothing();
 
@@ -56,29 +60,45 @@ public class GalleryFragment extends Fragment {
         if(tempUnits == "metric") {
             if (temp > 35) {
                 clothingText.setText("It's hot outside! Wear light clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.blank_t_shirt_png_30253);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp > 20){
                 clothingText.setText("It's cool outside. Enjoy the weather");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.blank_t_shirt_png_30253);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp > 5){
                 clothingText.setText("It's cold outside! Wear warm clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.pngtreeblank_sweater_apparel_6650587);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp < 5){
                 clothingText.setText("It's freezing! Wear warm clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.pngtreeblank_sweater_apparel_6650587);
+                clothingImage.setImageDrawable(myDrawable);
             }
         }
         else if(tempUnits == "imperial"){
             if (temp > 95) {
                 clothingText.setText("It's hot outside! Wear light clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.blank_t_shirt_png_30253);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp > 68){
                 clothingText.setText("It's cool outside. Enjoy the weather");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.blank_t_shirt_png_30253);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp > 41){
                 clothingText.setText("It's cold outside! Wear warm clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.pngtreeblank_sweater_apparel_6650587);
+                clothingImage.setImageDrawable(myDrawable);
             }
             else if (temp < 41){
                 clothingText.setText("It's freezing! Wear warm clothes.");
+                Drawable myDrawable = getResources().getDrawable(R.drawable.pngtreeblank_sweater_apparel_6650587);
+                clothingImage.setImageDrawable(myDrawable);
             }
         }
         else{
